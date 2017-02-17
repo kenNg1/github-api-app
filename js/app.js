cd0b3639c39d4fb8561abca3b420ec109425d96c
kenNg1 <kng1230@hotmail.com>
2017-02-17 16:18:20 +0800

    made api key available

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-var apiKey = require('./../.env').apiKey;
+// if i wanted to hide my API I would do this
+// var apiKey = require('./../.env').apiKey;
+
+var apiKey = 'e2d14a028d4e064a1ca04c8d1fb4ff2e4c8a3676';
 
 //function gitSearch(){};
 // replace above with the below..
