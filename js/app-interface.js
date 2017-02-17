2c3419cd138f1da974a9763cc1b71568200b0157
kenNg1 <kng1230@hotmail.com>
2017-02-17 12:16:41 +0800

    second commit

--- js/app-interface.js
+++ js/app-interface.js
@@ -3,30 +3,20 @@ var gitSearch = require('./../js/app.js').gitsearchmodule;
 
 $(document).ready(function(){
 
-//test
+//test//
 var currentGitSearch = new gitSearch();
 currentGitSearch.hello();
 
 
-//get info on click of button:
+// console.log(response);
+
+// get info on click of button:
+//
 $('#submit').click(function(event){
   event.preventDefault;
-  var response = {};
-  currentGitSearch.getUserRepos();
-
   $('ul.info').append('<li>'+$('#inputUser').val()+'</li>');
-
-  console.log(response);
-
+  currentGitSearch.getUserRepos();
 });
-
-
-
-
-
-
-
-
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 var apiKey = require('./../.env').apiKey;
 
-function gitSearch(){
-};
+function gitSearch(){};
 
 gitSearch.prototype.hello = function(){
-  console.log("hello");
+  console.log("hello there");
 }
 
-gitSearch.prototype.getUserRepos = function(){
-    var response = {};
-    $.get('https://api.github.com/users/'+$('#inputUser').val()+'?access_token=' + apiKey).then(function(response){
-    return response;
-
-
+  gitSearch.prototype.getUserRepos = function(){
+    $.get('https://api.github.com/users/kenNg1?access_token=' + apiKey).then(function(response){
+      console.log(response);
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
   };
+
+//https://api.github.com/users/kenNg1?access_token=e2d14a028d4e064a1ca04c8d1fb4ff2e4c8a3676
 
 exports.gitsearchmodule = gitSearch;
